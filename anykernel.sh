5de8a7b49a2d09bd9f36a1c4c41eed0527b8667a
Richard Raya <rdxzv.dev@gmail.com>
2026-06-30 13:45:42 +0700

    anykernel: Add legacy bootargs patch
    Signed-off-by: kylieeXD <khayloaftck@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -18,9 +18,58 @@ supported.versions=11-16
 ## AnyKernel file attributes
 # set permissions / ownership for included ramdisk files
 boot_attributes() {
-	set_perm_recursive 0 0 755 644 "$ramdisk/*";
-	set_perm_recursive 0 0 750 750 "$ramdisk/init*" "$ramdisk/sbin";
+    set_perm_recursive 0 0 755 644 "$ramdisk/*";
+    set_perm_recursive 0 0 750 750 "$ramdisk/init*" "$ramdisk/sbin";
 }
+
+# begin build.prop loader
+load_build_props() {
+    if [ ! -f "/system/build.prop" ] && [ ! -f "/system_root/system/build.prop" ]; then
+        mount /system 2>/dev/null || mount /system_root 2>/dev/null
+    fi
+
+    if [ -f "/system/build.prop" ]; then
+        SYSTEM_BUILD_PROP="/system/build.prop"
+    elif [ -f "/system_root/system/build.prop" ]; then
+        SYSTEM_BUILD_PROP="/system_root/system/build.prop"
+    fi
+
+    if [ -n "$SYSTEM_BUILD_PROP" ]; then
+        PROP_MIUI=$(file_getprop "$SYSTEM_BUILD_PROP" ro.miui.ui.version.code)
+    fi
+
+    if [ -f "$AKHOME/android_ver" ]; then
+        ANDROID_VERSION=$(cat "$AKHOME/android_ver")
+    fi
+} # end build.prop loader
+
+# begin legacy bootargs patch
+patch_legacy_bootargs() {
+    ui_print " "
+
+    if [ -n "$PROP_MIUI" ]; then
+        ui_print "MIUI $PROP_MIUI detected, defaulting to legacy bootargs"
+        patch_cmdline init.is_legacy_ebpf init.is_legacy_ebpf=1
+        patch_cmdline init.is_legacy_timestamp init.is_legacy_timestamp=1
+        return
+    fi
+
+    if [ "$ANDROID_VERSION" -lt 15 ]; then
+        ui_print "Enabling legacy eBPF bootarg..."
+        patch_cmdline init.is_legacy_ebpf init.is_legacy_ebpf=1
+    else
+        ui_print "Disabling legacy eBPF bootarg..."
+        patch_cmdline init.is_legacy_ebpf init.is_legacy_ebpf=0
+    fi
+
+    if [ "$ANDROID_VERSION" -lt 13 ]; then
+        ui_print "Enabling legacy timestamp bootarg..."
+        patch_cmdline init.is_legacy_timestamp init.is_legacy_timestamp=1
+    else
+        ui_print "Disabling legacy timestamp bootarg..."
+        patch_cmdline init.is_legacy_timestamp init.is_legacy_timestamp=0
+    fi
+} # end legacy bootargs patch
 
 # shell variables
 block="/dev/block/bootdevice/by-name/boot";
@@ -39,5 +88,7 @@ mv kernels/Image.gz Image.gz;
 
 ## AnyKernel install
 dump_boot;
+load_build_props;
+patch_legacy_bootargs;
 write_boot;
 ## end install
